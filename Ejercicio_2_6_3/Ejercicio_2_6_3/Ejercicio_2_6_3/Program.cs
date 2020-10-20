fc122b61d06f824df303bdd4fc7dc6e88e7af2bd
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 12:47:26 +0200

    Añadido nuevo ejercicio

---
+++ Ejercicio_2_6_3/Ejercicio_2_6_3/Ejercicio_2_6_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio_2_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            int numero3;
+            int multiplicacion;
+
+            Console.WriteLine("Introduce el primer número: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+         
+           /* do 
+            {
+                Console.WriteLine("Introduce el segundo número: ");
+                numero2 = Convert.ToInt32(Console.ReadLine());
+
+                i
+                Console.WriteLine("{0}", multiplicacion);
+
+
+            }*/
+        }
+    }
+}
