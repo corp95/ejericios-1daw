53313978758d03696038a5b25ab33aba2c0316a4
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 13:01:08 +0200

    Nuevo ejercicio

---
+++ Ejercicio_2_7_1/Ejercicio_2_7_1/Ejercicio_2_7_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_2_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            int anyo; //Es el año
+
+            try
+            {
+                Console.WriteLine("Introduce tu edad: ");
+                edad = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Introduce el año de nacimiento: ");
+                anyo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Pulsa cualquier botón para salir.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("El número introducido no es válido."); 
+            }
+        }
+    }
+}
