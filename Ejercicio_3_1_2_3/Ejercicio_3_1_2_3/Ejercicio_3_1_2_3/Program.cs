4c5975c2fc9b447c6ffa7e7ee08e6973a6f09a40
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 13:35:34 +0200

    Nuevo ejercicio

---
+++ Ejercicio_3_1_2_3/Ejercicio_3_1_2_3/Ejercicio_3_1_2_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_3_1_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num1;
+            long num2;
+            long suma;
+            long resta;
+            long producto;
+
+            Console.WriteLine("Introduce el primer número");
+            num1 = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo número");
+            num2 = Convert.ToInt64(Console.ReadLine());
+
+            suma = num1 + num2;
+            resta = num1 - num2;
+            producto = num1 * num2;
+
+            Console.WriteLine("La suma de {0} y {1} es: {2}. Su resa es {3} y su producto es {4}", num1, num2, suma, resta, producto);
+
+        }
+    }
+}
