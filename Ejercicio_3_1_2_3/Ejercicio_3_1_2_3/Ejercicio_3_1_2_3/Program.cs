48e62c0de19f7acd58fc182a7fa39f95daba3bf9
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 13:36:46 +0200

    Errores mínimos fixed

--- Ejercicio_3_1_2_3/Ejercicio_3_1_2_3/Ejercicio_3_1_2_3/Program.cs
+++ Ejercicio_3_1_2_3/Ejercicio_3_1_2_3/Ejercicio_3_1_2_3/Program.cs
@@ -22,7 +22,7 @@ namespace Ejercicio_3_1_2_3
             resta = num1 - num2;
             producto = num1 * num2;
 
-            Console.WriteLine("La suma de {0} y {1} es: {2}. Su resa es {3} y su producto es {4}", num1, num2, suma, resta, producto);
+            Console.WriteLine("La suma de {0} y {1} es: {2}. Su resta es {3} y su producto es {4}", num1, num2, suma, resta, producto);
 
         }
     }
