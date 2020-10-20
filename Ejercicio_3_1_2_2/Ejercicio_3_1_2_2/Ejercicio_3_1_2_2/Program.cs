43190da721b1ab2485928f86d4a89722d53a5e7b
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 13:30:17 +0200

    Ejercicio nuevo

---
+++ Ejercicio_3_1_2_2/Ejercicio_3_1_2_2/Ejercicio_3_1_2_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_3_1_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1;
+            byte num2;
+            ushort resultado;
+
+            Console.WriteLine("Introduce un número.");
+            num1 =Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Introduce un número.");
+            num2 =Convert.ToByte(Console.ReadLine());
+
+            resultado =(ushort) (num1 * num2);
+
+            Console.WriteLine("El resultado es {0}", resultado);
+
+
+        }
+    }
+}
