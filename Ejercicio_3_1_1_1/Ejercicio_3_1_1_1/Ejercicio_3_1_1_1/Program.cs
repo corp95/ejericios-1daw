3b125a56fca98f231613d0d07200ba00142fe826
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 13:15:53 +0200

    Ejercicio_3_1_1_1

---
+++ Ejercicio_3_1_1_1/Ejercicio_3_1_1_1/Ejercicio_3_1_1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio_3_1_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num1=1000000;
+            long num2=1000000;
+            long resultado=num1*num2;
+
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, resultado);
+
+            int num3 = 1000000;
+            int num4 = 1000000;
+            int resultado1 = num3 * num4;
+
+            Console.WriteLine("{0} * {1} = {2}", num3, num4, resultado1);
+
+        }
+    }
+}
