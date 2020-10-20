e5914230d3c12edd62662da65d50070916af186a
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 13:20:33 +0200

    Nuevo ejercicio

---
+++ Ejercicio_3_1_2_1/Ejercicio_3_1_2_1/Ejercicio_3_1_2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio_3_1_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte edad;
+
+            Console.WriteLine("¿Cuántos años tienes?");
+            edad = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Eh, no aparentas tener {0} años.", edad);
+        }
+    }
+}
