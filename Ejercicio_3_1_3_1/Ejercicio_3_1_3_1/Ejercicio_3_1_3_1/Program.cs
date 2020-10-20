e1d71692b6dbe18e48b736183543ade9af169d84
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-20 13:53:38 +0200

    Ejercicio nuevo

---
+++ Ejercicio_3_1_3_1/Ejercicio_3_1_3_1/Ejercicio_3_1_3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_3_1_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=15;
+            int y=-10;
+            int z=2147483647;
+            int a, b, c;
+
+            a = x++;
+            b = y++;
+            c = z++;
+
+
+            Console.WriteLine("{0} {1} {2}",a,b,c);
+        }
+    }
+}
