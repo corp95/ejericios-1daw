f5028b7ec00e525a6e1aa63c5070d9d643f54143
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-22 12:49:51 +0200

    Ejercicio nuevo

---
+++ Ejercicio_4_1_2_1/Ejercicio_4_1_2_1/Ejercicio_4_1_2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_4_1_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mes;
+               
+            int[] dias =    
+                {31,28,30,31,30,31,30,31,30,31,30,31};
+
+            Console.WriteLine("Introduce un día del mes (1=enero, 12=diciembre): ");
+            mes = Convert.ToInt32(Console.ReadLine());
+
+            mes--;
+
+            Console.WriteLine("El mes {0} tiene {1} días", mes+1, dias[mes]);
+        }
+    }
+}
