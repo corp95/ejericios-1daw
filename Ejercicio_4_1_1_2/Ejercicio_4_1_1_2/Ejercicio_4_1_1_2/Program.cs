f93632832255397d634a4d29bb55c3352ab27be0
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-22 12:25:17 +0200

    Nuevo ejercicio

---
+++ Ejercicio_4_1_1_2/Ejercicio_4_1_1_2/Ejercicio_4_1_1_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_4_1_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numeros = new float[5];
+
+            Console.WriteLine("Introduce el primero número: ");
+            numeros[0] = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo número: ");
+            numeros[1] = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Introduce el tercero número: ");
+            numeros[2] = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Introduce el cuarto número: ");
+            numeros[3] = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Introduce el quinto número: ");
+            numeros[4] = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", numeros[4], numeros[3], numeros[2], numeros[1], numeros[0]);
+        }
+    }
+}
