b6409be1c85101d3a26f5920363c9d7589717a7f
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-10-23 10:27:33 +0200

    Ejercicio nuevo

---
+++ Ejercicio_4_1_3_3/Ejeercicio_4_1_3_3/Ejeercicio_4_1_3_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejeercicio_4_1_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros = new double[10];
+            double media;
+            double suma=0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Introduce un número: ");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                suma = suma + numeros[i];
+            }
+
+            media = suma / 10;
+
+            Console.WriteLine("La media de los números introducidos es: {0}", media);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (media<numeros[i])
+                {
+                    Console.WriteLine("Los números {0} están por encima de la media.", numeros[i]);
+                }
+            }
+
+
+        }
+    }
+}
