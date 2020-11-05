bba18747d9789c510b1d3d23be8d7f7530742b7b
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-11-05 13:46:20 +0100

    Ejercicio pendiente

---
+++ Ejercicio_4_1_4_1/Ejercicio_4_1_4_1/Ejercicio_4_1_4_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_4_1_4_1
+{
+    class Program
+    {
+        static void Main(string[] args) //NO FUNCIONA, CONTINUAR
+        {
+            int[] datos = { 1, 10, 15, 91, 0 };
+            //int capacidad = 5;
+            int cantidad = 4;
+            int buscar;
+
+            for (int i=0; i<cantidad; i++)
+            {
+                Console.Write(" {0} ", datos[i]);
+                Console.WriteLine();
+            }
+
+            for (int i=0; i<cantidad; i++)
+            {
+                Console.WriteLine("Introduce el dato a buscar: ");
+                buscar = Convert.ToInt32(Console.ReadLine());
+
+                if (buscar == datos[i])
+                    Console.WriteLine("Número {0} encontrado", buscar);
+                break;
+            }
+
+        }
+    }
+}
