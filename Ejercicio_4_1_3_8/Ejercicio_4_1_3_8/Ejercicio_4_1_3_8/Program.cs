a42548b95eb17f06c06e91b9d9e91318f59fe6b8
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-11-05 12:17:03 +0100

    Ejercicio nuevo

---
+++ Ejercicio_4_1_3_8/Ejercicio_4_1_3_8/Ejercicio_4_1_3_8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_4_1_3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros = new double[3];
+            double resultado;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Introduce tres números: ");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            resultado = Math.Sqrt((numeros[0] * numeros[0]) + (numeros[1]) * (numeros[1]) + (numeros[2] * numeros[2]));
+
+            Console.WriteLine("{0}", resultado);
+           
+            
+        }
+    }
+}
