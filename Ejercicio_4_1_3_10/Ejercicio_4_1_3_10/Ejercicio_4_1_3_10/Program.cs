c8662ffb8a6784f7e9c5759c29eda18317ad44ea
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-11-05 12:34:44 +0100

    Ejercicio nuevo

---
+++ Ejercicio_4_1_3_10/Ejercicio_4_1_3_10/Ejercicio_4_1_3_10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_4_1_3_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vector1 = new double[3];
+            double[] vector2 = new double[3];
+            double resultado=0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Introduce tres números: ");
+                vector1[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Introduce otros tres números más: ");
+                vector2[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            resultado = (vector1[0] * vector2[0] + vector1[1] * vector1[1] + vector1[2] * vector2[2]);
+
+            Console.WriteLine("El resultado es {0}", resultado);
+        }
+    }
+}
