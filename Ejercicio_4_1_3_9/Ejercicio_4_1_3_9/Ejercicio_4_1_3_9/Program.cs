f779c0a004d5ab7e9e16d61dc970c9588fc6f447
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-11-05 12:28:46 +0100

    Ejercicio nuevo

---
+++ Ejercicio_4_1_3_9/Ejercicio_4_1_3_9/Ejercicio_4_1_3_9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace Ejercicio_4_1_3_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vector1 = new double[3];
+            double[] vector2 = new double[3];
+            double suma1=0;
+            double suma2 = 0;
+            double suma3 = 0;
+            double sumatotal;
+
+            for(int i=0;i<3;i++)
+            {
+                Console.WriteLine("Introduce tres números: ");
+                vector1[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            
+
+            for(int i=0;i<3;i++)
+            {
+                Console.WriteLine("Introduce otros tres números más: ");
+                vector2[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            suma1 = vector1[0] + vector2[0];
+            suma2 = vector1[1] + vector2[1];
+            suma3 = vector1[2] + vector2[2];
+            sumatotal = suma1 + suma2 + suma3;
+
+            Console.WriteLine("Las sumas de manera individual son: {0}, {1}, {2}. El total es: {3}", suma1, suma2, suma3, sumatotal);
+
+        }
+    }
+}
