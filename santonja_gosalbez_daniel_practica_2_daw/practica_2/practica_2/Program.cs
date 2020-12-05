246086571f27cbdd59da5d31b36abaa0b8700032
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-12-05 16:20:32 +0100

    YA FUNCIONA Con comentarios incluidos

--- santonja_gosalbez_daniel_practica_2_daw/practica_2/practica_2/Program.cs
+++ santonja_gosalbez_daniel_practica_2_daw/practica_2/practica_2/Program.cs
@@ -14,15 +14,15 @@ namespace practica_2
 	{
 		static void Main(string[] args)
 		{
-			string cadena = "0";
+			string cadena = "0"; //La cadena donde vamos a introducir la palabra a adivinar. 
 			char letra = '▄'; //Le asignamos ese valor raro porque así nos aseguramos que nadie lo va a introducir.
-			int contadorIntentos = 0;
-			bool letraEncontrada = false;
+			int contadorIntentos = 0; //En esta variable vamos a ir acumulando los intentos.
+			bool letraEncontrada = false; //En esta variable vamos a ver si la letra que hemos introducido forma parte de la cadena o no.
 
-			Console.Write("Cadena: ");
+			Console.Write("Cadena: "); //Pedimos al usuario que introduzca una palabra y la guardamos en la variable.
 			cadena = Convert.ToString(Console.ReadLine());
 
-			StringBuilder cadOculta = new StringBuilder(cadena);
+			StringBuilder cadOculta = new StringBuilder(cadena); //Hacemos un StringBuilder donde vamos a meter la cadena oculta por '_'
 
 			for (int i = 0; i < cadOculta.Length; i++) //Esto es para convertir la palabra en guiones.
 			{
@@ -34,18 +34,19 @@ namespace practica_2
 				letra = '▄'; //Volvemos a ponerlo para inicializarlo.
 
 				while (letra== '▄') //Este while me va a servir por si el usuario introduce un valor vacío.
-                {
+				{
 					Console.Write("Letra: ");
 
 					try
 					{
 						letra = Console.ReadLine()[0];	/*Con esta línea cogemos sólo la primera letra introducida, 
-														 *así no se nos rompe el programa*/
+														 *así no se nos rompe el programa si el usuario introduce más de una letra.*/
 					}
 					catch (Exception)
-                    {
-						Console.WriteLine("Tienes que introducir al menos una letra.");	//En el caso de que ponga un valor vacío, me lo pide de nuevo.					
-                    }
+					{
+						Console.WriteLine("Tienes que introducir al menos una letra.");	/*En el caso de que ponga un valor vacío, 
+																						 * me lo pide de nuevo.*/			
+					}
 				}
 
 				for (int i = 0; i < cadena.Length; i++)  /*Este for se utiliza para recorrer la cadena y 
@@ -53,19 +54,19 @@ namespace practica_2
 														  * si coincide*/
 				{
 					if (cadena[i] == char.ToUpper(letra) || cadena[i] == char.ToLower(letra)) /* Compruebo mayuscula y
-																							* minuscula y pongo la
-																							* letra original*/
+																							   * minuscula y pongo la
+																							   * palabra original*/
 					{
 						letraEncontrada = true;
 						cadOculta[i] = cadena[i];
 					}
 				}
 
-				if (letraEncontrada == false) //Si no hemos encontrado ninguna letra, sumamos.
+				if (letraEncontrada == false) //Si no hemos encontrado ninguna letra, nos metemos en este if.
 				{
 
-					switch(contadorIntentos) //Utilizamos un switch para ir dibujando y sumando intentos fallidos.
-                    {
+					switch(contadorIntentos) //Utilizamos un switch para ir dibujando.
+					{
 						case 0:
 							Console.WriteLine("┌───┐\n│   o\n│\n│\n│\n┴───────\n");
 							break;
@@ -86,7 +87,7 @@ namespace practica_2
 							break;
 					}
 
-					contadorIntentos++;
+					contadorIntentos++; //Y añadimos un intento al contador.
 
 				}
 
@@ -94,19 +95,20 @@ namespace practica_2
 
 				for (int i = 0; i < cadOculta.Length; i++) //Este for es para mostrar los '_' con espacios entre si.
 				{
-					Console.Write("{0} ", cadOculta[i]);
+					Console.Write("{0} ", cadOculta[i]); //Ponemos un espacio después de {0} para que se vea con un espacio entre cada uno.
 
 				}
 				Console.WriteLine();
 
 			}
 
-			while ((contadorIntentos < 6) && (cadOculta.ToString() != cadena));
+			while ((contadorIntentos < 6) && (cadOculta.ToString() != cadena)); /* Si se cumple la condición de que el contador es menor
+																				 * que 6 o lo que ponemos coincide con la palabra sale del do*/
 			{
-				if (cadOculta.ToString() == cadena)
+				if (cadOculta.ToString() == cadena) // Si lo que ponemos coincide con la cadena te dice enhorabuena.
 					Console.WriteLine("¡¡¡ENHORABUENA!!!");
 
-				else
+				else //De otra manera te dice lo siguiente:
 					Console.WriteLine("R.I.P. La palabra era: {0}", cadena);
 			}
 
