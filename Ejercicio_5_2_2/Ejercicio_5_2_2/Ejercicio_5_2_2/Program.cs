ff7f083733e2a7b2420e37df4ee7d2b0c34dd86a
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-12-17 12:28:24 +0100

    Ejercicio nuevo

---
+++ Ejercicio_5_2_2/Ejercicio_5_2_2/Ejercicio_5_2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_5_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DibujarCuadrado3x3();
+        }
+
+        public static void DibujarCuadrado3x3()
+        {
+            for (int i=0; i<3; i++)
+            Console.WriteLine("* * *");
+        }
+    }
+}
