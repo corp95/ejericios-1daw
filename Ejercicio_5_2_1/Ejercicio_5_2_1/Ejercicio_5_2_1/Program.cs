41c288a7f18b52086d0d2702b7e513106c266104
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-12-17 12:19:31 +0100

    Ejercicio nuevo

---
+++ Ejercicio_5_2_1/Ejercicio_5_2_1/Ejercicio_5_2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio_5_2_1
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pulsa intro para borrar pantalla.");
+            Console.ReadLine();
+
+            BorrarPantalla();
+        }
+        public static void BorrarPantalla()
+        {
+            // Se puede hacer con un Console.Clear(); y es mejor. Hay que intentar usar herramientas del sistema.
+            for (int i = 0; i < 25; i++)
+                Console.WriteLine();
+        }
+
+    }
+}
