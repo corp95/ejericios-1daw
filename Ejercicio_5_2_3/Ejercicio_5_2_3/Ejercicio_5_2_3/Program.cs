33a63578f0d2fb1c2bdae34973b33442fd94b01b
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-12-18 09:15:42 +0100

    Ejercicio nuevo

---
+++ Ejercicio_5_2_3/Ejercicio_5_2_3/Ejercicio_5_2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio_5_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IMPORTANTE HACER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        }
+
+        public static void 
+    }
+}
---
+++ Ejercicio_5_3_1/Ejercicio_5_3_1/Ejercicio_5_3_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio_5_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int alto;
+            int ancho;
+
+            Console.WriteLine("Introduce el alto: ");
+            alto = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el ancho: ");
+            ancho = Convert.ToInt32(Console.ReadLine());
+
+            DibujarCuadrado(alto, ancho);
+
+        }
+
+        public static void DibujarCuadrado(int x, int y)
+        {
+            Console.WriteLine();
+
+            for(int i=0; i<x; i++) //Alto
+            {                               
+                for (int j=0; j<y; j++) //Ancho
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
