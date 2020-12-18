a141b1d2e5b33bdc9af249b585a8095cfe35bc84
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2020-12-18 09:23:27 +0100

    Ejercicio nuevo

---
+++ Ejercicio_5_3_2/Ejercicio_5_3_2/Ejercicio_5_3_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio_5_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ancho;
+            int alto;
+
+            Console.WriteLine("Introduce el ancho: ");
+            ancho = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce el alto: ");
+            alto = Convert.ToInt32(Console.ReadLine());
+
+            DibujarRectangulo(alto, ancho);
+        }
+
+        public static void DibujarRectangulo(int x, int y)
+        {
+            Console.WriteLine();
+
+            for(int i=0; i<x; i++)
+            {
+                for(int j=0; j<y; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
