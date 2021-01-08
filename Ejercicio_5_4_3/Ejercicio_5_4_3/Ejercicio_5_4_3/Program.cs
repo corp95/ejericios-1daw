b5f72a4181a85bc8c7fe9d9e960808746d25cc49
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2021-01-08 09:32:21 +0100

    Ejercicio nuevo

---
+++ Ejercicio_5_4_3/Ejercicio_5_4_3/Ejercicio_5_4_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_5_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Introduce un número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Signo(num));
+
+        }
+
+        static int Signo(int num)
+        {
+            Console.WriteLine("");
+
+            if (num == 0)
+                return 0;
+            else
+                if (num > 0)
+                return 1;
+            else
+                return -1;
+         
+        }
+    }
+}
