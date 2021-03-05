9c6701d406c86945626bca6f77f4fdd972a6c50d
Daniel Santonja Gosálbez <santonjagosalbezdaniel@gmail.com>
2021-03-05 09:56:25 +0100

    ejercicio nuevo

---
+++ Ejercicio_6_3_1/Ejercicio_6_3_1/Ejercicio_6_3_1/Puerta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6_3_1
+{
+    class Puerta
+    {
+        public int alto;
+        private int Ancho;
+        private string color;
+
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+        
+
+        }
+        public string GetColor()
+        {
+            return this.color;
+        }
+        /// <summary>
+        /// Poner ancho. Si es menor de 75 y mayor de 120 saltará una excepción.
+        /// </summary>
+        public void SetAncho(int ancho)
+        {
+            if (Ancho < 75 || Ancho > 120)
+            {
+                throw new ArgumentException("Error. Ancho incorrecto.");
+            }
+            else
+                this.Ancho =  ancho;
+        }
+    }
+}
---
+++ Ejercicio_6_3_2/Ejercicio_6_3_2/Ejercicio_6_3_2/persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6_3_2
+{
+    class persona
+    {
+        private string nombre;
+
+        public void SetPersona(string nombre)
+        {
+            this.nombre = nombre;   
+        }
+
+        public string GetPersona()
+        {
+            return this.nombre;
+        }
+        
+        public string Saludar()
+        {
+            return "Hola, soy " + this.nombre;
+        }
+    }
+}
